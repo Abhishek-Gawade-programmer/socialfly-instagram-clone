542e414935c87f5f66f5e0d5e8d32af594985253
Abhishek Gawade <supeduperr.oa@gmail.com>
2021-06-28 23:15:59 +0530

    addde some changes tu login page

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from users.models import *
+
+admin.site.register(User)
+admin.site.register(SocialflyUser)
 
 # Register your models here.
--- users/models.py
+++ users/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib import messages
 
-# Create your models here.
+class User(AbstractUser):
+    is_genuine = models.BooleanField(default=False)
+    # def __str__(self):
+    #     return  str(self.get_full_name())
+
+
+class SocialflyUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    followers = models.ManyToManyField(User,related_name='followers')
+    following = models.ManyToManyField(User,related_name='following')
+    phone_number=models.CharField(max_length=20)
+    description=models.TextField(max_length=300)
+
+
+
+
+    def save(self, *args, **kwargs):
+        self.class_teacher_roll = self.class_teacher_roll.upper()
+        super().save(*args, **kwargs)
+
+
+
+
+
+
+from django.contrib.auth.signals import user_logged_out
+
+def logout_notifier(sender, request, user, **kwargs):
+	messages.info(request, f"{request.user.get_full_name()} Logout Successfully ")
+   
+
+user_logged_out.connect(logout_notifier)
+
+
+
+
+
+
+
+
